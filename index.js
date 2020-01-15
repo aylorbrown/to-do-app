fa5099a2c51eca7fc4f570279ba83885b38d4a53
Aylor Brown <aylorbrown@gmail.com>
2020-01-15 16:02:36 -0500

    creats route paths

--- index.js
+++ index.js
@@ -5,9 +5,35 @@ const server = http.createServer(app);
 
 const db = require('./models/connection');
 
-console.log(db);
+const es6Renderer = require('express-es6-template-engine');
+app.engine('html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
+const bodyParser = require('body-parser');
+const parseForm = bodyParser.urlencoded({
+    extended: true
+});
 
 const PORT = 3000;
+
+//home 
+app.get('/', (req, res) => {
+    console.log('You are home!');
+    res.render('home');
+});
+
+//login 
+app.get('/login', (req, res) => {
+    res.render('form');
+});
+
+app.post('/login', parseForm, (req, res) => {
+    const { firstName, lastName, password } = req.body;
+    console.log(req.body);
+});
+
+//logout 
 
 
 
