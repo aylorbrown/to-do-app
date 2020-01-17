69a81c4fb420c0e1b5c3506a01bb13aa56cbc74e
Travis Franklin <travis.william.franklin@gmail.com>
2020-01-17 11:26:50 -0500

    adds view events page, create events page

---
+++ models/events.js
@@ -0,0 +1,21 @@
+const db = require('./connection');
+
+async function listEvents(){
+    const result = await db.any(`
+    select * from events`);
+    console.log(result);
+    return result;
+}
+
+async function createEvent(eventName, eventLocation, eventDate, eventTime, eventDescription){
+    const result = await db.result(`
+    insert into events
+        (event_name, event_location, event_date, event_time, event_description)
+    values ($1, $2, $3, $4, $5)
+    `, [eventName, eventLocation, eventDate, eventTime, eventDescription]);
+}
+
+module.exports= {
+    listEvents,
+    createEvent
+}
