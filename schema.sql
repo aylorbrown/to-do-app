086e911baf5e2cceeffa9033b0cf1d54ac9eead9
Catie Evans <catieevans@Caties-MacBook-Pro.local>
2020-01-20 11:21:21 -0500

    adds multiple task creation template

--- schema.sql
+++ schema.sql
@@ -5,8 +5,8 @@ create TABLE users (
     organization_name text,
     email text,
     phone_number VARCHAR(12),
-    user_name text unique not null,
-    password text   
+    user_name text unique NOT NULL,
+    password text NOT NULL  
 );
 
 create TABLE events (
@@ -15,19 +15,23 @@ create TABLE events (
     event_location text,
     event_date date,
     event_time time,
-    event_description text 
+    event_description text,
+    user_id INTEGER REFERENCES users(user_id) NOT NULL
+    -- NOT NULL: Forces every event to have an user id, otherwise event cannot be created
 );
 
-create TABLE userID_eventID (
-    userID_eventID serial PRIMARY key,
-    user_id INTEGER REFERENCES users (user_id),
-    event_id INTEGER REFERENCES events (event_id)
-);
 
 create TABLE tasks (
     task_id serial PRIMARY key,
-    event_id INTEGER REFERENCES events(event_id),
-    user_id INTEGER REFERENCES users(user_id),
-    task text
+    task text,
+    event_id INTEGER REFERENCES events(event_id) NOT NULL
+    -- NOT NULL: Forces every task to have an event id, otherwise task cannot be created
+
 );
 
+
+create TABLE task_assignment (
+    task_assignment_id serial PRIMARY key,
+    task_id INTEGER REFERENCES tasks (task_id) NOT NULL,
+    user_id INTEGER REFERENCES users (user_id) NOT NULL 
+);
