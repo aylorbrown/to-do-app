9add29bf100531376c67d259b02fceab2595b7d4
Travis Franklin <travis.william.franklin@gmail.com>
2020-01-21 15:03:10 -0500

    fixes buttons, inserts favicon, adds language to about page

--- index.js
+++ index.js
@@ -273,7 +273,11 @@ app.get('/logout', (req, res) => {
 
 
 
+//----About page--Travis//
 
+app.get('/about', (req, res) => {
+    res.render('about.html');
+})
 
 
 
--- public/styles.js
+++ public/styles.js
@@ -1,7 +1,7 @@
 // when a user clicks on the button the text changes to thanks 
 
-const changeButtonText = document.getElementById("mybtn")
+const changeButtonText = document.getElementById("mybtn");
 
 changeButtonText.addEventListener("click", () => {
     changeButtonText.innerHTML = "Thanks!";
-})
+});
